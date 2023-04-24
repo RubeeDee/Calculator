2c73e4fc26c8fcdd2e770a8f10512dfa5a9ffdc8
RubeeDee <rubeearchi@gmail.com>
2023-04-24 20:26:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var sum = ()=> {
+    var first = prompt('enter first number')
+    var second = prompt('enter second number')
+    var sum = Number(first) + Number(second)
+    alert('the sum is ' +sum)
+}
+
+var subtraction = ()=> {
+    var first = prompt('enter first number')
+    var second = prompt('enter second number')
+    var subtraction = Number(first) - Number(second)
+    alert('the answer is ' +subtraction)
+}
+
+var multiplication = ()=> {
+    var first = prompt('enter first number')
+    var second = prompt('enter second number')
+    var multiplication = Number(first) * Number(second)
+    alert('the answer is ' +multiplication)
+}
+
+var division = ()=> {
+    var first = prompt('enter first number')
+    var second = prompt('enter second number')
+    var division = Number(first) / Number(second)
+    alert('the answer is ' +division)
+}
